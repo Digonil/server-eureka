d0a3352c8d1b26b76986f24d19f366adc55a9b0d
Diego Niles <diego.niles@hotmail.com>
2023-02-04 18:16:53 -0300

    Criado service discovery com Eureka

---
+++ src/test/java/br/com/alurafood/serverms/ServerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alurafood.serverms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
